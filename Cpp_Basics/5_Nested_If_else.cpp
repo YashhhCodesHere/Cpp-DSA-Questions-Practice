721624f6cf0ceb1a50485c249fa3ebfc12856010
Yash Kumar <Yashxhere@gmail.com>
2024-09-01 20:06:05 +0530

    feat: Add nested if-else logic for age eligibility and retirement

---
+++ Cpp_Basics/5_Nested_If_else.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age < 18){
+        cout << "You're NOT eligible for work!\n";
+    }
+    else if(age>=18){
+        cout << "You're eligible for work!\n";
+        if(age>=55 && age <57){
+            cout << "You're eligible for work, but Yours retirement is soon!\n";
+        }
+    }
+        if(age > 57){
+            cout << "Retirement time!\n";
+        }
+}

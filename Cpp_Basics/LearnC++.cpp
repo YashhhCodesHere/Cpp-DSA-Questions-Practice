94066465a6310d0fcf4e39fb79572c6e5ef86743
Yash Kumar <Yashxhere@gmail.com>
2024-09-01 16:35:43 +0530

    feat: Add input and output functionality to C++ program
    The commit updates the C++ code file "LearnC++.cpp" by adding input and output functionality. It prompts the user to enter values for variables x and y, and then prints the values of x, y, and their sum to the console. This enhancement improves the functionality of the C++ program for learning purposes.

--- Cpp_Basics/LearnC++.cpp
+++ Cpp_Basics/LearnC++.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-using namespace std;
+using namespace std;   // This will include the standard library to all the builtin function.
+
 int main()
 {
-    // cout << "Hey Yashhh\n" << "Leo Leo" << endl << "Good Morning" << endl;
-    int x;
+    int x, y;
+    cout << "Enter the value of x: ";
     cin >> x;
-    cout << "The value of x is: " << x;
+    cout << "Enter the value of y: ";
+    cin >> y;
+    cout << "The value of x is: " << x << " & The value of y is: " << y << endl;
+    cout << "Their sum is: " << (x+y);
+
+    return 0;
 }

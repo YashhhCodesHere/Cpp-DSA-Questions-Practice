16ce0ae83f5d226939cf572d9868857546ef58e4
Yash Kumar <Yashxhere@gmail.com>
2024-09-02 09:52:19 +0530

    chore: Add 1D and 2D array handling to C++ code file
    The commit adds handling for 1D and 2D arrays in the C++ code file "7_Array.cpp". It allows users to input values into a 1D array and modifies the value of an element in the array. Additionally, it creates a 2D array of size 3x5. This enhancement improves the functionality of the program and provides support for array operations.

--- Cpp_Basics/7_Array.cpp
+++ Cpp_Basics/7_Array.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
+    // 1D Array:-
+    int _1D_Arr[5];    // This line will gonna make an array of size = 5 whose name will be 'input_number'
+    // Unassigned array stores garbage values inside it!
+    cin >> _1D_Arr[0] >> _1D_Arr[1] >> _1D_Arr[2] >> _1D_Arr[3] >> _1D_Arr[4];
+
+    _1D_Arr[2] += 15;  // Modifying array's value!
+    cout << _1D_Arr[3];
     
+    // 2D Array:-   (Widely used while solving martix problems or graphs problems)
+    int _2D_Arr[3][5];  // This line will create a 2D array of size 3x5, Where 3 denotes Rows & 5 denotes Column!
+
+
     return 0;
 }

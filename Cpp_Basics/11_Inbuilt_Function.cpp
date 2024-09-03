722e1e0c7e432cd20dd29924cb08ac5e37f50324
Yash Kumar <Yashxhere@gmail.com>
2024-09-03 05:48:15 +0530

    Refactor C++ code files for better organization

--- Cpp_Basics/11_Inbuilt_Function.cpp
+++ Cpp_Basics/11_Inbuilt_Function.cpp
@@ -4,6 +4,8 @@ int main()
 {
     int num1 = 23, num2 = 14;
     int maximum = max(num1, num2);  // Shows maximum of two inputs!
-    cout << maximum;
+    cout << "Maximum of both number is: " << maximum;
+
+
     return 0;
 }

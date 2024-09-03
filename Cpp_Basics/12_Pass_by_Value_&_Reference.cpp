baf69b0988e486c132120713abc999c8e80844d8
Yash Kumar <Yashxhere@gmail.com>
2024-09-03 06:15:01 +0530

    Refactor C++ code files for better organization

---
+++ Cpp_Basics/12_Pass_by_Value_&_Reference.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int pass_by_value(int a){
+    a += 10;
+    return a;   // This will gonna make a copy of 'a' and return with adding 10 to it!
+}
+int pass_by_reference(int &a){
+    a += 10;
+    return a;   // This will take the original address of 'a' and add 10 to it!
+}
+int main()
+{
+    int a = 5;
+    cout << pass_by_value(a) << endl;
+    cout << a << "\n";  // Original value of 'a' will remains same!
+    cout << pass_by_reference(a) << endl;
+    cout << a << "\n";
+    return 0;
+}

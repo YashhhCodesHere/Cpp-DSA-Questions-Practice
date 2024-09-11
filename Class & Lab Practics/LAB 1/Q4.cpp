bd7f1078f21045919b3ea8fa1e92b657636a970a
Yash Kumar <Yashxhere@gmail.com>
2024-09-12 00:07:25 +0530

    Refactor C++ code to reverse an array

--- Class & Lab Practics/LAB 1/Q4.cpp
+++ Class & Lab Practics/LAB 1/Q4.cpp
@@ -17,20 +17,14 @@ int main() {
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
-
-    int start = 0;
-    int end = size - 1;
-    while (start < end) {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
+    int rev_arr[size];
+    for(int i=0;i<size;i++){
+        rev_arr[i]=arr[size-i-1];
     }
-
     cout << "\nReversed array: ";
     for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+        cout << "The '" << i << "'th elements of the reversed array: ";
+        cout << rev_arr[i] << " ";
     }
 
     return 0;

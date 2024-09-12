f91c80fd484a7d69b052be22ac5b60a5b3c22390
Yash Kumar <Yashxhere@gmail.com>
2024-09-12 08:48:57 +0530

    Refactor C++ code to add typecasting functionality

---
+++ Cpp Basics/13_Typecasting.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // Data can be converted in two types:-
+    // 1. Implicit Typecasting (Type conversion): Automatically done by the compiler(Conversion of smaller data type to larger data type)
+    // 2. Explicit Typecasting: Manually done by the programmer(Conversion of larger data type to smaller data type)
+    
+    float num = 101.999;
+    cout<<num<<endl;
+    num = int(num);
+    cout<<num;
+
+    return 0;
+}

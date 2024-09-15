f07214fc1e0e2123b2e2eac91fe7e72be7cb92dc
Yash Kumar <Yashxhere@gmail.com>
2024-09-15 20:14:09 +0530

    Refactor C++ code to add functionality for getting the size of an array

---
+++ Arrays/Get_Size_Of_Array.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {100,21,42,95,88};  // We don't need to declare the size of the array if we are initializing it's value at the same time!
+
+    // To get the size of array:-
+    int size = sizeof(arr) / sizeof(int);
+    cout<<"The size of the array is: "<<size<<endl;
+    return 0;
+}

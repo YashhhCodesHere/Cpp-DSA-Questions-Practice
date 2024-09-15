bebf08e9e74a5ffb4b895d6938b7aae89881d9e0
Yash Kumar <Yashxhere@gmail.com>
2024-09-15 21:03:35 +0530

    Refactor C++ code to find the minimum and maximum values in an array

--- Arrays/2_Min_Max_in_Array.cpp
+++ Arrays/2_Min_Max_in_Array.cpp
@@ -4,13 +4,19 @@ int main()
 {
     int arr[] = {5, 15, 22, 1, -15, 24};
 
-    int max_num = arr[0];
-    int min_num = arr[0];
-    for(int i = 0;i<(sizeof(arr)/sizeof(int));i++){
-        max_num = max(max_num,arr[i]);
-        min_num = min(min_num,arr[i]);
+    int max_num = arr[0], max_index = 0;
+    int min_num = arr[0], min_index = 0;
+    for(int i = 0; i < (sizeof(arr)/sizeof(int)); i++){
+        if(arr[i] > max_num) {
+            max_num = arr[i];
+            max_index = i;
+        }
+        if(arr[i] < min_num) {
+            min_num = arr[i];
+            min_index = i;
+        }
     }
-    cout<<"Maximum element of array is: "<< max_num<<endl;
-    cout<<"Minimum element of array is: "<< min_num<<endl;
+    cout<<"Maximum element of array is: "<< max_num<< " at the index: "<< max_index<<endl;
+    cout<<"Minimum element of array is: "<< min_num<< " at the index: "<< min_index<<endl;
     return 0;
 }

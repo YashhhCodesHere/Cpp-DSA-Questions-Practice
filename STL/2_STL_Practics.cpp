c0fff422f2a515f739f197f2b4158349b6ca5dfc
Yash Kumar <Yashxhere@gmail.com>
2024-09-18 11:44:38 +0530

    Refactor C++ code to add pair example and improve vector declaration and usage

--- STL/2_STL_Practics.cpp
+++ STL/2_STL_Practics.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
+#include <utility>
 using namespace std;
 
 int main()
 {
+    pair<int,int> p = {3,5};
+    cout << p.first<<"\n";
 
+    //Nested Pair:-
+    pair<int, pair<int,int>> nested_pair = {1, {4,5}};
+    cout<< nested_pair.second.first;
+
+    // Array in the form of Pairs:-
+    pair<int, int> arr[] = {{1,2}, {2,4}, {4,8}};
+    cout<< arr[1].second;
     return 0;
 }
--- Vector/1_Vector.cpp
+++ Vector/1_Vector.cpp
@@ -13,6 +13,10 @@ int main()
         cout<<val<<" ";
     }
 
+    for(auto val : arr){    // For printing all the vector elements when the datatype is not known:
+        cout<<val<<" ";
+    }
+
     // 3rd Method:-
     vector<int> arr1(5, 0);    // 5 elements all with value 0
 
@@ -20,6 +24,7 @@ int main()
     cout<<"\nThe size of the 'vec' is: "<<vec.size()<<endl;     // Prints the Size of vector
     vec.push_back(5);    // Adds element 5 to the vector 'vec'
     vec.push_back(10);    // Adds element 10 to the vector 'vec'
+    vec.emplace_back(50);   // Adds element 50 to the vector 'vec' But it is faster than push_back!
     vec.push_back(20);    // Adds element 20 to the vector 'vec'
     cout<<"The 0th element of the vec: "<<vec[0]<<endl;  
     vec.pop_back();     // Removes the last element of the vector
@@ -28,4 +33,10 @@ int main()
     cout<<"For printing the value at index 1: "<<vec.at(1)<<endl;    // Prints the value at index 1
     cout<<"For checking if the vector is empty or not: "<<vec.empty()<<endl;    // Returns 1 if empty else 0
     vec.clear();    // Clears the vector"
+
+    // Calling the vector elements using iterator:-
+
+    vector<int>::iterator it = arr.begin();    // Declaring an iterator named 'it' and pointing it to the beginning of the vector
+    it++;    // Incrementing the iterator
+    cout<<"Printing the second element of the vector using iterator: "<< *it <<endl;    // Prints the second element of the vector
 }

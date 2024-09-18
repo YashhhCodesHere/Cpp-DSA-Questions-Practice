fab51b782f0c231669630cd56a79c94830547e55
Yash Kumar <Yashxhere@gmail.com>
2024-09-18 11:08:11 +0530

    Refactor C++ code to improve STL examples and add missing includes

--- STL/1_STL_Examples_with_Syntax.cpp
+++ STL/1_STL_Examples_with_Syntax.cpp
@@ -1,156 +1,124 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
 using namespace std;
-int main(){
 
-    // Standard Template Library (STL) in C++:
-
-    // 1. Vector:
-    #include <vector>
-    // Syntax: vector<T> vec;
-    // Functions: push_back(), pop_back(), size(), clear(), empty(), at(), front(), back()
-    // Example:
+void vectorExample() {
     vector<int> vec;
     vec.push_back(1);
     vec.push_back(2);
     int size = vec.size();
     int first = vec.front();
     int last = vec.back();
+}
 
-    // 2. List:
-    #include <list>
-    // Syntax: list<T> lst;
-    // Functions: push_back(), push_front(), pop_back(), pop_front(), size(), clear(), empty(), front(), back()
-    // Example:
+void listExample() {
     list<int> lst;
     lst.push_back(1);
     lst.push_front(2);
     int size = lst.size();
     int first = lst.front();
     int last = lst.back();
+}
 
-    // 3. Deque:
-    #include <deque>
-    // Syntax: deque<T> deq;
-    // Functions: push_back(), push_front(), pop_back(), pop_front(), size(), clear(), empty(), at(), front(), back()
-    // Example:
+void dequeExample() {
     deque<int> deq;
     deq.push_back(1);
     deq.push_front(2);
     int size = deq.size();
     int first = deq.front();
     int last = deq.back();
+}
 
-    // 4. Stack:
-    #include <stack>
-    // Syntax: stack<T> stk;
-    // Functions: push(), pop(), size(), empty(), top()
-    // Example:
+void stackExample() {
     stack<int> stk;
     stk.push(1);
     stk.push(2);
     int top = stk.top();
     stk.pop();
+}
 
-    // 5. Queue:
-    #include <queue>
-    // Syntax: queue<T> que;
-    // Functions: push(), pop(), size(), empty(), front(), back()
-    // Example:
+void queueExample() {
     queue<int> que;
     que.push(1);
     que.push(2);
     int front = que.front();
     int back = que.back();
     que.pop();
+}
 
-    // 6. Priority Queue:
-    #include <queue>
-    // Syntax: priority_queue<T> pq;
-    // Functions: push(), pop(), size(), empty(), top()
-    // Example:
+void priorityQueueExample() {
     priority_queue<int> pq;
     pq.push(1);
     pq.push(2);
     int top = pq.top();
     pq.pop();
+}
 
-    // 7. Set:
-    #include <set>
-    // Syntax: set<T> st;
-    // Functions: insert(), erase(), size(), clear(), empty(), find(), count(), begin(), end()
-    // Example:
+void setExample() {
     set<int> st;
     st.insert(1);
     st.insert(2);
     int size = st.size();
     auto it = st.find(1);
     st.erase(it);
+}
 
-    // 8. Multiset:
-    #include <set>
-    // Syntax: multiset<T> mst;
-    // Functions: insert(), erase(), size(), clear(), empty(), find(), count(), begin(), end()
-    // Example:
+void multisetExample() {
     multiset<int> mst;
     mst.insert(1);
     mst.insert(2);
     int size = mst.size();
     auto it = mst.find(1);
     mst.erase(it);
+}
 
-    // 9. Map:
-    #include <map>
-    // Syntax: map<Key, T> mp;
-    // Functions: insert(), erase(), size(), clear(), empty(), find(), count(), begin(), end(), at(), []
-    // Example:
+void mapExample() {
     map<int, int> mp;
     mp[1] = 2;
     mp.insert({3, 4});
     int size = mp.size();
     auto it = mp.find(1);
     mp.erase(it);
+}
 
-    // 10. Multimap:
-    #include <map>
-    // Syntax: multimap<Key, T> mmp;
-    // Functions: insert(), erase(), size(), clear(), empty(), find(), count(), begin(), end()
-    // Example:
+void multimapExample() {
     multimap<int, int> mmp;
     mmp.insert({1, 2});
     mmp.insert({3, 4});
     int size = mmp.size();
     auto it = mmp.find(1);
     mmp.erase(it);
+}
 
-    // 11. Unordered Set:
-    #include <unordered_set>
-    // Syntax: unordered_set<T> us;
-    // Functions: insert(), erase(), size(), clear(), empty(), find(), count(), begin(), end()
-    // Example:
+void unorderedSetExample() {
     unordered_set<int> us;
     us.insert(1);
     us.insert(2);
     int size = us.size();
     auto it = us.find(1);
     us.erase(it);
+}
 
-    // 12. Unordered Map:
-    #include <unordered_map>
-    // Syntax: unordered_map<Key, T> ump;
-    // Functions: insert(), erase(), size(), clear(), empty(), find(), count(), begin(), end(), at(), []
-    // Example:
+void unorderedMapExample() {
     unordered_map<int, int> ump;
     ump[1] = 2;
     ump.insert({3, 4});
     int size = ump.size();
     auto it = ump.find(1);
     ump.erase(it);
+}
 
-    // 13. Algorithm:
-    #include <algorithm>
-    #include <numeric>
-    // Functions: sort(), reverse(), max(), min(), accumulate(), count(), find(), binary_search(), lower_bound(), upper_bound()
-    // Example:
+void algorithmExample() {
     vector<int> vec = {1, 2, 3, 4, 5};
     sort(vec.begin(), vec.end());
     reverse(vec.begin(), vec.end());
@@ -162,6 +130,22 @@ int main(){
     bool found = binary_search(vec.begin(), vec.end(), 3);
     auto lb = lower_bound(vec.begin(), vec.end(), 3);
     auto ub = upper_bound(vec.begin(), vec.end(), 3);
+}
+
+int main() {
+    vectorExample();
+    listExample();
+    dequeExample();
+    stackExample();
+    queueExample();
+    priorityQueueExample();
+    setExample();
+    multisetExample();
+    mapExample();
+    multimapExample();
+    unorderedSetExample();
+    unorderedMapExample();
+    algorithmExample();
 
     return 0;
 }

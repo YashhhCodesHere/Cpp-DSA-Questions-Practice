e85deb6bc656c6328337911e0851ca4f407dbf61
Yash Kumar <Yashxhere@gmail.com>
2024-09-23 11:29:47 +0530

    Refactor C++ code to remove unused variable and print value using cout

---
+++ Cpp Basics/15_Pointers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // & (Ampersand) --> is used to get the address of a variable
+    // * --> (Dereference Operator)is used to get the value at the address
+
+    int a = 5;
+    int* b = &a;    
+    cout<<b;
+
+    return 0;
+}
--- Cpp Basics/Cpp_Starting_Syntax.cpp
+++ Cpp Basics/Cpp_Starting_Syntax.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 int main()
 {
-    int a = 5;
-    // int* b = &a;
-    cout << a;
+
     return 0;
 }

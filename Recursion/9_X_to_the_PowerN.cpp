1ba962b8c9f4ca4a439095957167e930a1c8e732
Yash Kumar <Yashxhere@gmail.com>
2024-10-04 00:20:52 +0530

    Refactor C++ code for calculating X to the power N recursively and optimize it
    - Add a new function xToPowerOptimised() to calculate x to the power n recursively
    - Modify the main() function to take user input for num and power
    - Print the result of both xToPower() and xToPowerOptimised() functions

--- Recursion/9_X_to_the_PowerN.cpp
+++ Recursion/9_X_to_the_PowerN.cpp
@@ -1,8 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int xToPower(int x, int n){
+    if(n==1){
+        return x;
+    }
+    return x * xToPower(x, n-1);
+}
+
+int xToPowerOptimised(int x, int n){
+    if(n==1){
+        return x;
+    }
+    if(n%2 == 0){
+        return xToPowerOptimised(x, n/2) * xToPowerOptimised(x, n/2);
+    }
+    return x * xToPowerOptimised(x, n-1);
+} 
 int main()
 {
+    int num, power;
+    cout<<"Enter the value of n & power: ";
+    cin>>num>>power;
+    cout<<"The value of "<< num <<" raised to the power "<< power <<" is: "<<xToPower(num, power)<<endl;
     
+    cout<<"The value of "<< num <<" raised to the power "<< power <<" is: "<<xToPowerOptimised(num, power)<<endl;
     return 0;
 }

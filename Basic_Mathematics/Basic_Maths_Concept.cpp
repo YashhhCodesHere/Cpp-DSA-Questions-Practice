256f27f35c0f02935bafcdda76806c6e77b56465
Yash Kumar <Yashxhere@gmail.com>
2024-10-03 09:51:25 +0530

    Add Basic_Maths_Concept.cpp file with reverse number and digit count functionality

---
+++ Basic_Mathematics/Basic_Maths_Concept.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 7789;
+    int reverse = 0;
+    while(n>0){
+        int lastDigit = n%10;   // Extracting the last digit of the number
+        reverse = reverse*10 + lastDigit;   // Adding the last digit to the reverse number
+        n = n/10;   // Removing the last digit from the number
+    }
+    cout<< "The reverse of the number is: "<< reverse << endl;
+
+    int no = log10(n) + 1; // Number of digits in the original number
+    cout<< "The number of digits in the original number is: "<< no << endl;
+    return 0;
+}

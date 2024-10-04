bd382c4fe807deca442a7d43de602b49f6e516bb
Yash Kumar <Yashxhere@gmail.com>
2024-10-04 23:09:19 +0530

    Refactor C++ code to add input validation for array elements and increase the size of the hash table for array hashing
    Refactor C++ code to add array hashing functionality and palindrome checking with reverse number counting
    
    Refactor C++ code for calculating X to the power N recursively and optimize it

--- Hashing/1_Array_Hashing.cpp
+++ Hashing/1_Array_Hashing.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// hash is pre-defined in C++ STL, so we can't use it as a variable name.
+int hashh[100000000000000];    // Declaring a Hash Table of size 10^14 globally which will not give stack overflow error or segmentation fault.
+// Globally, we don't need to initialize the hash table, it will be initialized by 0 by default.
 int main()
 {
     int n;
@@ -8,14 +10,15 @@ int main()
     cin>>n;
     int arr[n];
     cout<<"Enter the elements of the array: ";
-    
+
     for(int i = 0; i < n; i++){
         cin>>arr[i];
     }
+
     // Precompute Hash Table:-
-    int hash[100] = {0};
+    // int hashh[100] = {0};    -> This will give stack overflow error or segmentation fault for large size of array.
     for(int i = 0; i < n; i++){
-        hash[arr[i]] += 1;
+        hashh[arr[i]] += 1;
     }
     
     int q;
@@ -25,7 +28,7 @@ int main()
         int number;
         cout<<"Enter the number you want to know, how much time it occurs in array: ";
         cin>>number;
-        cout<<number<< " occurs "<<hash[number]<<" times in the array!"<<endl;
+        cout<<number<< " occurs "<<hashh[number]<<" times in the array!"<<endl;
     }
     return 0;
 }

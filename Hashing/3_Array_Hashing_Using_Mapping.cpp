8e2bc398c3b0ac89c8d9e3036831cc14e78c89e4
Yash Kumar <Yashxhere@gmail.com>
2024-10-05 12:35:34 +0530

    Refactor C++ code to add input validation for array elements and increase the size of the hash table for array hashing

--- Hashing/3_Array_Hashing_Using_Mapping.cpp
+++ Hashing/3_Array_Hashing_Using_Mapping.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// Unordered map is preferred over map because it has O(1) time complexity for insertion, deletion and searching.
 int main()
 {
     int n;
@@ -13,14 +13,14 @@ int main()
     }
 
 // Pre-Compute:-
-map<int, int> mpp;
+unordered_map<int, int> mpp;  // Declaring a map globally, which will stores the frequency of each element of the array as key-value pair.
 for(int i = 0; i < n; i++){
     mpp[arr[i]]++;
 }
 
 // Iterating the components of map:
 for(auto it : mpp){
-    cout<< it.first << " -> "<< it.second<<endl;
+    cout<< it.first << " -> "<< it.second<<endl;    // Map is sorted in increasing order of keys, While unordered_map is not sorted.
 }
 
 int q;
--- Linked List/1_Creating_LinkedList.cpp
+++ Linked List/1_Creating_LinkedList.cpp
@@ -1,50 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class node{
-    public:
+class node
+{
+public:
     int data;
-    node* next;
+    node *next;
 
-    node(int val){
+    node(int val)
+    {
         data = val;
         next = NULL;
     }
-};
-
-void insertAtHead(node* &head, int val){
-    node* n = new node(val);
-    n->next=head;
-    head = n;
-}
-
-void insertAtTail(node* &head, int val){
-    node* n = new node(val);
-
-    if(head == NULL){
+    void insertAtHead(node *&head, int val)
+    {
+        node *n = new node(val);
+        n->next = head;
         head = n;
-        return;
     }
 
-    node* temp = head;
-    while(temp -> next!=NULL){
-        temp = temp->next;
-    }
-    temp -> next = n;
-}
+    void insertAtTail(node *&head, int val)
+    {
+        node *n = new node(val);
 
-void display(node* head){
-    node* temp = head;
-    while(temp!=NULL){
-        cout<< temp->data<<" -> ";
-        temp = temp->next;
+        if (head == NULL)
+        {
+            head = n;
+            return;
+        }
+
+        node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = n;
     }
-    cout<<"NULL"<<endl;
-}
+
+    void display(node *head)
+    {
+        node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
 
 int main()
 {
-    node* head = NULL;
+    node *head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
     insertAtTail(head, 3);

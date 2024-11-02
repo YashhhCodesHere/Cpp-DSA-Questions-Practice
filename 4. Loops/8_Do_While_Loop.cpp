61ba6607a686d5b3f5999a246525582c16ba6e97
Yash Kumar <Yashxhere@gmail.com>
2024-11-02 15:46:07 +0530

    Add example and explanation for do-while loop implementation

--- 4. Loops/8_Do_While_Loop.cpp
+++ 4. Loops/8_Do_While_Loop.cpp
@@ -1,8 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+    Do-While Loop: It is used to iterate over a block of code as long as the condition is true.
+    Syntax:
+        // initialization
+        do
+        {
+            // code
+            //update the condition
+        }while(condition);
+    
+    *The block of code is executed at least once, even if the condition is false.
+*/
 int main()
 {
-    
+    int i = 10;
+    cout << "Printing numbers from 10 to 25:\t";
+    do
+    {
+        cout << i << " ";
+        i++;
+    }while(i <= 25);
+
+    // Semi-colon is necessary after the while loop.
     return 0;
 }

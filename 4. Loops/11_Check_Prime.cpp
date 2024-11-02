ca6ce66fb668444304ade6ee2a056c9ab5bf0947
Yash Kumar <Yashxhere@gmail.com>
2024-11-02 20:26:27 +0530

    Add prime number checking functionality

--- 4. Loops/11_Check_Prime.cpp
+++ 4. Loops/11_Check_Prime.cpp
@@ -6,6 +6,18 @@ int main()
     int num;
     cout << "Enter the number: ";
     cin >> num;
-    
+    bool isPrime = true;
+    for(int i = 2; i <= sqrt(num); i++){
+        if(num%i == 0 || num%(num-i) == 0){
+            isPrime = false;
+            break;
+        }
+    }
+
+    if(isPrime){
+        cout << num << " is Prime!\n";
+    }else{
+        cout << num << " is NOT Prime!\n";
+    }
     return 0;
 }

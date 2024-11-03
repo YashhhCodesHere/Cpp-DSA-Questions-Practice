35598ef9592607560d8bbcb2aaa4febb82fccc7c
Yash Kumar <Yashxhere@gmail.com>
2024-11-03 12:29:08 +0530

    Add star and inverted star pattern printing functionality

--- 5. Pattern/2_Star_and_Inverted_Star_Pattern.cpp
+++ 5. Pattern/2_Star_and_Inverted_Star_Pattern.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i+1; j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    
+    cout << "\n";
+    
+    for(int i = 0; i < n; i++){
+        for (int j = n; j > i ; j--){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    
     
     return 0;
 }

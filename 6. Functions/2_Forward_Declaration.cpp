9b62add8579235b4a605a3974236a9897d6252ed
Yash Kumar <Yashxhere@gmail.com>
2024-11-03 14:35:00 +0530

    Add forward declaration example with function implementation

--- 6. Functions/2_Forward_Declaration.cpp
+++ 6. Functions/2_Forward_Declaration.cpp
@@ -4,8 +4,16 @@ using namespace std;
     Forward Declaration: The declaration of a function before its definition including the return type, function name, and parameters if any.
     Function Definition: The actual implementation & body of the function.
 */
+
+void sayHello();    // Forward Declaration
+
 int main()
 {
-    
+    sayHello();
     return 0;
+}
+
+void sayHello()
+{
+    cout << "Hello World" << endl;
 }

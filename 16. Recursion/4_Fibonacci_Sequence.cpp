073881f11bcb88c71a35f546c0695a082c5a99ed
Yash Kumar <Yashxhere@gmail.com>
2024-11-06 10:29:52 +0530

    Add Fibonacci sequence calculation using recursion; include user input and output

--- 16. Recursion/4_Fibonacci_Sequence.cpp
+++ 16. Recursion/4_Fibonacci_Sequence.cpp
@@ -1,12 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int fibonacci(int n){
+    if(n <= 1){
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
 int main()
 {
     int n;
     cout << "Enter the number: ";
     cin >> n;
     
+    cout << n << "th Fibonacci Number is: " << fibonacci(n) << "\n";
     
     return 0;
 }

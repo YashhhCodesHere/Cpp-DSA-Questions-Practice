0d6bf5bddfee1ab84f656c14d03cdf11793e0877
Yash Kumar <Yashxhere@gmail.com>
2025-01-01 13:03:27 +0530

    Add print_array function and enhance changeArr for backtracking demonstration

--- 19. BackTracking/1_BT_on_arrays.cpp
+++ 19. BackTracking/1_BT_on_arrays.cpp
@@ -1,15 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void changeArr(int *arr, int n, int i){
+void print_array(int *arr, int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
 
+void changeArr(int *arr, int n, int i){
+    if(i == n){
+        cout << "Array when base case hits: ";
+        print_array(arr, n);
+        return;
+    }
+    arr[i] = i+1;
+    changeArr(arr, n, i+1);
+    arr[i] -= 2;
 }
 
 int main(){
     int arr[5] = {0};
     int n = 5;
 
-    changeArr()
+    changeArr(arr, n, 0);
+
+    cout << "Array when backtracking ends: ";
+    print_array(arr, n);
 
     return 0;
 }

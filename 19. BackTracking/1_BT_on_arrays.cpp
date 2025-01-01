facb7ffc87024225807181fc08987543645eaaa0
Yash Kumar <Yashxhere@gmail.com>
2025-01-01 12:50:37 +0530

    Add initial implementation of backtracking on arrays with a placeholder function

---
+++ 19. BackTracking/1_BT_on_arrays.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void changeArr(int *arr, int n, int i){
+
+}
+
+int main(){
+    int arr[5] = {0};
+    int n = 5;
+
+    changeArr()
+
+    return 0;
+}

57ed28a33e8232373b52b9a53e0c882fa6c92893
Yash Kumar <Yashxhere@gmail.com>
2025-01-19 23:57:58 +0530

    Implement stack using linked list with template support

--- 21. Stack/4_Stack_implementation_using_LinkedList_Node.cpp
+++ 21. Stack/4_Stack_implementation_using_LinkedList_Node.cpp
@@ -1,7 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+template <class T>
+class Node {
+public:
+    T data;
+    Node* next;
+
+    Node(T val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+template <class T>
+class Stack {
+    Node<T>* head; // Use the template type <T> for the Node pointer
+public:
+    Stack() {
+        head = nullptr;
+    }
+
+    void push(T val) {
+        Node<T>* newNode = new Node<T>(val);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void pop() {
+        if (head == nullptr) {
+            cout << "Can't POP -> Stack is Empty!\n";
+            return;
+        }
+        Node<T>* toDelete = head;
+        head = head->next;
+
+        toDelete->next = nullptr;
+        delete toDelete;
+    }
+
+    T top() {
+        if (isEmpty()) {
+            throw runtime_error("Stack is empty. No top element.");
+        }
+        return head->data;
+    }
+
+    bool isEmpty() {
+        return (head == nullptr);
+    }
+};
+
+int main() {
+    Stack<int> s; // Use Stack<int> for integer stack
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    while (!s.isEmpty()) {
+        cout << "Top: " << s.top() << endl;
+        s.pop();
+    }
+
+    // Trying to pop from an empty stack
+    s.pop();
+    s.top();
+
     return 0;
 }

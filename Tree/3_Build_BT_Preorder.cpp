53d9c85f414a8178fe61aaafd90383b95fdbe1ee
Yash Kumar <Yashxhere@gmail.com>
2025-01-21 22:55:28 +0530

    Implement binary tree structure with TreeNode class

---
+++ Tree/3_Build_BT_Preorder.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+public: 
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+class BinaryTree{
+public:
+    TreeNode buildTree(vector<int> nodes)
+};
+
+int main(){
+    
+    return 0;
+}

eb0e5b01e55d5774076062b37f8f32cab893748f
Yash Kumar <Yashxhere@gmail.com>
2025-01-25 21:10:13 +0530

    Add comments to explain deque usage in queue implementation

--- 22. Queue/4_Queue_using_Deque.cpp
+++ 22. Queue/4_Queue_using_Deque.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+    Here, we are using deque to implement queue.
+    Some member functions of deque are:
+    1. push() -  dq.push_back(val)  |   dq.push_front(val)
+    2. pop() -   dq.pop_back()      |   dq.pop_front()
+    3. front() - dq.front()         |   dq.back()
+*/
+
 class Queue{
     deque<int> dq;
 public:

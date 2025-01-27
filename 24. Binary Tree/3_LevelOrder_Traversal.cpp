dc01f4e9edbb1add1ca811641e0851677561b3bc
Yash Kumar <Yashxhere@gmail.com>
2025-01-27 12:30:20 +0530

    Implement level order traversal for binary tree with and without null markers

---
+++ 24. Binary Tree/3_LevelOrder_Traversal.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+TreeNode* buildTree(vector<int> nodes, int& idx){
+    if(idx >= nodes.size() || nodes[idx] == -1){
+        idx++;
+        return nullptr;
+    }
+
+    TreeNode* currNode = new TreeNode(nodes[idx++]);
+    currNode -> left = buildTree(nodes, idx);
+    currNode -> right = buildTree(nodes, idx);
+
+    return currNode;
+}
+
+void levelOrderTraversal(TreeNode* root){
+    if(!root){
+        return;
+    }
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode* curr = q.front();
+        q.pop();
+
+        cout << curr -> data << " ";
+
+        if(curr -> left){
+            q.push(curr -> left);
+        }
+        
+        if(curr -> right){
+            q.push(curr -> right);
+        }
+    }
+}
+
+
+void levelOrderTraversalWithNull(TreeNode* root){
+    if(!root){
+        return;
+    }
+
+    queue<TreeNode*> q;
+    q.push(root);
+    q.push(nullptr);
+
+    while(!q.empty()){
+        TreeNode* curr = q.front();
+        q.pop();
+
+        if(curr == nullptr){
+            cout << "\n";
+            if(q.empty()){
+                return;
+            }
+            q.push(nullptr);
+        }else{
+            cout << curr -> data << " ";
+
+            if(curr -> left){
+                q.push(curr -> left);
+            }
+        
+            if(curr -> right){
+                q.push(curr -> right);
+            }
+        }
+    }
+}
+
+int main(){
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1};
+    int idx = 0;
+
+    TreeNode* root = buildTree(nodes, idx);
+
+    levelOrderTraversal(root);
+    cout << "\n\nLevel-Wise BFS:-\n";
+
+    levelOrderTraversalWithNull(root);
+    cout << "\n";
+
+    return 0;
+}

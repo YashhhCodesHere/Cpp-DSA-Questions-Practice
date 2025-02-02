999c6636efa1e111f72ffe674e607f2fc1b0ca6c
Yash Kumar <Yashxhere@gmail.com>
2025-02-03 01:05:32 +0530

    Implement insert and build functions for binary search tree

--- 25. BST (Binary Search Tree)/1_Insert&Build_BST.cpp
+++ 25. BST (Binary Search Tree)/1_Insert&Build_BST.cpp
@@ -13,7 +13,44 @@ using namespace std;
     2. Worst Case: O(n) -> Skewed BST
 */
 
+class TreeNode{
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+TreeNode* insert(TreeNode* root, int val){
+    if(!root){
+        return (new TreeNode(val));
+    }
+
+    if(val < root->data){
+        root->left = insert(root->left, val);
+    }else if(val > root->data){
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+TreeNode* buildBST(vector<int> arr){
+    TreeNode* root = nullptr;
+
+    for(int i=0; i<arr.size(); i++){
+        root = insert(root, arr[i]);
+    }
+
+    return root;
+}
+
 int main(){
-    
+    vector<int> values = {10, 5, 15, 3, 7, 12, 18};
+    TreeNode* root = buildBST(values);
+
     return 0;
 }

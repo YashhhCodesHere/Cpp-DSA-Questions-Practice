b90c196b5a8f015c8864a2b976fb99f71edec64a
Yash Kumar <Yashxhere@gmail.com>
2025-02-11 23:11:09 +0530

    Add explanation for insert() function return values in unordered_set

--- 27. Hashing/5_Sets.cpp
+++ 27. Hashing/5_Sets.cpp
@@ -5,6 +5,11 @@ using namespace std;
 /*
     Sets are also same as unordered_set but they store the keys in sorted order, and it's implementation is just like maps.
     Time Complexity of Set is same as Maps Operations, i.e., O(log n).
+
+    The insert() function in unordered_set returns a pair<iterator, bool>;
+    where:-
+        iterator → Points to the inserted element or the existing element if it was already present.
+        bool → Indicates whether the insertion was successful (true if inserted, false if it was already there).
 */
 
 int main(){
